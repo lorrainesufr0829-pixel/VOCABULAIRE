6ac4b2d7b634e3bbe8cb95d250985398c04a27fd
lorrainesufr0829-pixel <lorrainesufr0829@gmail.com>
2026-06-23 20:20:33 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
